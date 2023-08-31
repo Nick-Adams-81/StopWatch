d3653d66c746dd4f8786943878359be06620594f
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-31 13:48:39 -0700

    added global vars

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -4,8 +4,27 @@ import javax.swing.*;
 
 public class StopWatch implements ActionListener {
 
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Reset");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    boolean started = false;
+    String secondsString = String.format("%02d", seconds);
+    String minutesString = String.format("%2d", minutes);
+    String hoursString = String.format("%2d", hours);
+
     StopWatch() {
 
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+
+        frame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
