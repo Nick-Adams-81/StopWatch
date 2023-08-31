416045ec4a1bc0e385c84c7d085f3fc594116b53
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-31 13:42:40 -0700

    setup done

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,3 +1,26 @@
-public class StopWatch {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+public class StopWatch implements ActionListener {
+
+    StopWatch() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start() {
+
+    }
+
+    void stop() {
+
+    }
+
+    void reset() {
+
+    }
 }
